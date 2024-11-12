b561a38c41895ab6a31d7d142bc9a5bd3f22dc1f
Wonchan Kim <wonchankim000827@gmail.com>
2024-11-11 18:36:36 -0800

    Files configured for the project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React from 'react';
+import Header from './components/Header';
+import AboutSection from './components/AboutSection'
+import Tutorial from './components/RecipeSection'
+import Footer from './components/Footer'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      
+      <main className="container mx-auto px-4 flex flex-col items-center mt-10">
+        <AboutSection />
+        <Tutorial />
+      </main>
+
+      <footer>
+        <Footer />
+      </footer>
     </div>
   );
 }
 
 export default App;
+
