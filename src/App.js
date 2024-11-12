7877f4c2b02020c22a68fc80bdb8653d68f0030b
Ines <iim@sfu.ca>
2024-11-11 20:29:57 -0800

    added automated tests to yml file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React from 'react';
+import Header from './components/Header';
+import AboutSection from './components/AboutSection'
+import Tutorial from './components/RecipeSection'
+import Footer from './components/Footer'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          PlanYourPlate website in progress.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+
+      <main className="container mx-auto px-4 flex flex-col items-center mt-10">
+        <AboutSection />
+        <Tutorial />
+      </main>
+
+      <footer>
+        <Footer />
+      </footer>
     </div>
   );
 }
