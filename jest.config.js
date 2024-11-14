df0c6f99033449f61b8027fd596172954c9061e9
Wonchan Kim <wonchankim000827@gmail.com>
2024-11-13 20:37:22 -0800

    json files updated for stability

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+// jest.config.js
+module.exports = {
+  // ... other configurations
+  transformIgnorePatterns: [
+    "/node_modules/(?!(axios)/)"
+  ],
+  transform: {
+    "^.+\\.(js|jsx)$": "babel-jest"
+  },
+  // Use ESM extensions
+  extensionsToTreatAsEsm: [".jsx", ".js"]
+};
+
