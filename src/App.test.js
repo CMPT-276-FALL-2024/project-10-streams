8acbbf36bc314598da1aabc264b90307cec2172d
Ines <iim@sfu.ca>
2024-11-18 21:54:23 -0800

    added App.js, App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
+// src/App.test.js
+
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders PlanYourPlate heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headingElement = screen.getByText(/PlanYourPlate/i);
+  expect(headingElement).toBeInTheDocument();
 });
+
